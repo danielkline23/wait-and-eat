5f82a83cacceff753910e97ef6acf31c149eba28
Petar Petrovic <contact@petarpetrovic.co>
2016-09-26 21:33:20 +0200

    Added minor UI change and beautified code

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,33 +1,33 @@
 (function() {
   "use strict"
-  
+
   angular
     .module("app.auth")
     .controller("AuthController", AuthController);
-  
+
   AuthController.$inject = ["$location", "authService"];
-  
+
   function AuthController($location, authService) {
     var vm = this;
-    
+
     vm.error = null;
-    
+
     vm.register = register;
     vm.login = login;
-    
+
     function register(restaurant) {
       return authService.register(restaurant)
         .then(function() {
           vm.login(restaurant);
         })
-      .then(function() {
-        return authService.sendWelcomeEmail(restaurant.email);
-      })
+        .then(function() {
+          return authService.sendWelcomeEmail(restaurant.email);
+        })
         .catch(function(error) {
           vm.error = error;
         });
     }
-    
+
     function login(restaurant) {
       return authService.login(restaurant)
         .then(function(restaurant) {
@@ -38,7 +38,7 @@
           vm.error = error;
         });
     }
-    
+
   }
-  
+
 })();
